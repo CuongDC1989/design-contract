d796e7cc5559288de7d856d9e64df1590e1907eb
CuongDC <cuongdc@solashi.com>
2026-05-12 13:25:26 +0700

    feat: add check set constants

---
+++ src/constants.mjs
@@ -0,0 +1,5 @@
+export const CHECKS_STRICT    = ['exists', 'size', 'radius', 'background', 'border', 'shadow', 'opacity', 'layout', 'typography', 'text', 'overflow', 'blend'];
+export const CHECKS_CONTAINER = ['exists', 'size', 'radius', 'background', 'shadow', 'layout', 'overflow'];
+export const CHECKS_LAYOUT    = ['exists', 'size', 'layout'];
+export const CHECKS_SHAPE     = ['exists', 'size', 'radius', 'background'];
+export const CHECKS_ROW       = ['exists', 'size', 'layout'];
